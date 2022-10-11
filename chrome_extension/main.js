35429b7ee19af2c8490f5dbd44227354de0c7b20
Ryan <leehahnwoong@gmail.com>
2022-10-10 21:29:44 -0700

    [add] chrome extension codes

--- chrome_extension/main.js
+++ chrome_extension/main.js
@@ -1,1 +1,24 @@
-//This is a new folder for chrome extension support
+var currentTime = new Date();
+async function testData(){
+    var assignment_names = [];
+    classIds = [48117, 49725, 48939, 47295];
+    for (let i = 0; i < classIds.length; i++){
+        var temp_assignments = [];
+        var course = String(classIds[i]);
+        var res1 = await fetch(`https://canvas.eee.uci.edu/api/v1/courses/${course}/assignments?access_token=${token}`);
+        var record1 = await res1.json();
+        //console.log(course)
+        for (let j = 0; j < 10; j++){
+            var assignment_name = record1[j]?.name;
+            var assignment_date = record1[j]?.due_at;
+            if (assignment_date > currentTime){
+                if (assignment_name != undefined){
+                    temp_assignments.push(assignment_name);
+                }
+            }
+        }
+        assignment_names.push(temp_assignments);
+    }
+    console.log(assignment_names);
+}
+testData();
---
+++ chrome_extension/script.js
@@ -0,0 +1,51 @@
+async function fetchData(access_token) {
+  var token = String(access_token);
+  var courses = `https://cors-anywhere.herokuapp.com/https://canvas.eee.uci.edu/api/v1/courses?include[]=term&per_page=50&access_token=${token}`;
+  var currentTime = new Date();
+  var currentMonth = currentTime.getMonth()+1;
+  var currentYear = String(currentTime.getFullYear());
+  const classes = [];
+  const classIds = [];
+
+  if (currentMonth < 4){
+    var termMonth = "Winter"
+  } else if (currentMonth < 7){
+    var termMonth = "Spring"
+  } else if (currentMonth < 13){
+    var termMonth = "Fall"
+  }
+  var term = `${termMonth} ${currentYear}`
+  const res=await fetch (courses,{mode: 'cors'});
+  const record=await res.json();
+  for (let i = 0; i < record.length; i++) {
+    var term_name = record[i].term?.name;
+    var class_name=record[i].name;
+    var class_id=record[i].id;
+    if (term_name == term){
+      classes.push(class_name);
+      classIds.push(class_id)
+    }
+  }
+  console.log(classes);
+  console.log(classIds);
+  const assignment_names = []
+  for (let i = 0; i < classIds.length; i++){
+    const temp_assignments = [];
+    for (let j = 0; j < classIds.length; j++){
+      var course = classIds[i];
+      console.log(course)
+      var assignments = `https://cors-anywhere.herokuapp.com/https://canvas.eee.uci.edu/api/v1/courses/${course}/assignments?access_token=${token}`;
+      var res1 = await fetch(assignments);
+      var record1 = await res1.json();
+      var assignment_name = record1[j].name;
+      temp_assignments.push(assignment_name);
+    }
+    assignment_names.push(assignment_name)
+  }
+  console.log(assignment_names)
+}
+
+function buttonToken(){
+  const token_input = document.getElementById("token").value;
+  return fetchData(token_input);
+}
